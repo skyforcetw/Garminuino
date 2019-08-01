12fd9bfb77000f37affb487a58817b52234d9a9b
skyforce <skyforce@gmail.com>
2019-08-01 20:19:13 +0800

    add traffic & lane detection option in UI

--- GoogleMaps_HUD/gmaps_hud/src/main/java/sky4s/garminhud/app/MainActivity.java
+++ GoogleMaps_HUD/gmaps_hud/src/main/java/sky4s/garminhud/app/MainActivity.java
@@ -59,6 +59,36 @@ import sky4s.garminhud.eUnits;
 import sky4s.garminhud.hud.DummyHUD;
 import sky4s.garminhud.hud.HUDInterface;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.OrientationEventListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 public class MainActivity extends AppCompatActivity {
     //for test with virtual device which no BT device
     public final static boolean IGNORE_BT_DEVICE = (null == BluetoothAdapter.getDefaultAdapter());
@@ -648,6 +678,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
 
+            case R.id.switchTrafficAndLane:
+                break;
+
 
             case R.id.switchShowETA:
                 sendBooleanExtraByBroadcast(
@@ -859,6 +892,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //===============================================================================================
+    // location
+    //===============================================================================================
     private boolean locationServiceConnected;
     private boolean useLocationService;
     private LocationService locationService;
@@ -966,6 +1002,141 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //===============================================================================================
+    // screen capture
+    //===============================================================================================
+//    private static final String TAG = ScreenCaptureImageActivity.class.getName();
+    private static final int REQUEST_CODE = 100;
+    private static String STORE_DIRECTORY;
+    private static int IMAGES_PRODUCED;
+    private static final String SCREENCAP_NAME = "screencap";
+    private static final int VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC;
+    private static MediaProjection sMediaProjection;
+
+    private MediaProjectionManager mProjectionManager;
+    private ImageReader mImageReader;
+    private Handler mHandler;
+    private Display mDisplay;
+    private VirtualDisplay mVirtualDisplay;
+    private int mDensity;
+    private int mWidth;
+    private int mHeight;
+    private int mRotation;
+    private OrientationChangeCallback mOrientationChangeCallback;
+
+    private static long lastUpdateTime=0;
+    public final static long UpdateInterval = 1000;
+
+    private class ImageAvailableListener
+            implements ImageReader.OnImageAvailableListener {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = null;
+            FileOutputStream fos = null;
+            Bitmap bitmap = null;
+
+
+            try {
+                image = reader.acquireLatestImage();
+                if (image != null) {
+                    long currentTime = System.currentTimeMillis();
+                    if(currentTime- lastUpdateTime > UpdateInterval) {
+                        lastUpdateTime = currentTime;
+                        Image.Plane[] planes = image.getPlanes();
+                        ByteBuffer buffer = planes[0].getBuffer();
+                        int pixelStride = planes[0].getPixelStride();
+                        int rowStride = planes[0].getRowStride();
+                        int rowPadding = rowStride - pixelStride * mWidth;
+
+                        // create bitmap
+                        bitmap = Bitmap.createBitmap(mWidth + rowPadding / pixelStride, mHeight, Bitmap.Config.ARGB_8888);
+                        bitmap.copyPixelsFromBuffer(buffer);
+
+                        // write bitmap to a file
+                        fos = new FileOutputStream(STORE_DIRECTORY + "/myscreen_" + IMAGES_PRODUCED + ".png");
+                        bitmap.compress(CompressFormat.PNG, 100, fos);
+
+                        IMAGES_PRODUCED++;
+                        Log.e(TAG, "captured image: " + IMAGES_PRODUCED);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+
+                if (image != null) {
+                    image.close();
+                }
+            }
+        }
+    }
+
+    private class OrientationChangeCallback extends OrientationEventListener {
+
+        OrientationChangeCallback(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            final int rotation = mDisplay.getRotation();
+            if (rotation != mRotation) {
+                mRotation = rotation;
+                try {
+                    // clean up
+                    if (mVirtualDisplay != null) mVirtualDisplay.release();
+                    if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
+
+                    // re-create virtual display depending on device width / height
+                    createVirtualDisplay();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class MediaProjectionStopCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            Log.e("ScreenCapture", "stopping projection.");
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mVirtualDisplay != null) mVirtualDisplay.release();
+                    if (mImageReader != null) mImageReader.setOnImageAvailableListener(null, null);
+                    if (mOrientationChangeCallback != null) mOrientationChangeCallback.disable();
+                    sMediaProjection.unregisterCallback(MediaProjectionStopCallback.this);
+                }
+            });
+        }
+    }
+
+    /****************************************** Factoring Virtual Display creation ****************/
+    private void createVirtualDisplay() {
+        // get width and height
+        Point size = new Point();
+        mDisplay.getSize(size);
+        mWidth = size.x;
+        mHeight = size.y;
+
+        // start capture reader
+        mImageReader = ImageReader.newInstance(mWidth, mHeight, PixelFormat.RGBA_8888, 2);
+        mVirtualDisplay = sMediaProjection.createVirtualDisplay(SCREENCAP_NAME, mWidth, mHeight, mDensity, VIRTUAL_DISPLAY_FLAGS, mImageReader.getSurface(), null, mHandler);
+        mImageReader.setOnImageAvailableListener(new ImageAvailableListener(), mHandler);
+    }
 
 }
 
