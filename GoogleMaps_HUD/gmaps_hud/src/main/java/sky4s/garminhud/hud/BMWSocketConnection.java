cb3f68ded99a5347cc22a1edcefc9a91dea00c41
Ethan Chen <intervigil@gmail.com>
2020-08-29 00:45:37 -0700

    BMWSocketConnection: Add length based bytesToHex overload

--- GoogleMaps_HUD/gmaps_hud/src/main/java/sky4s/garminhud/hud/BMWSocketConnection.java
+++ GoogleMaps_HUD/gmaps_hud/src/main/java/sky4s/garminhud/hud/BMWSocketConnection.java
@@ -241,8 +241,12 @@ public class BMWSocketConnection {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
+        return bytesToHex(bytes, bytes.length);
+    }
+
+    private static String bytesToHex(byte[] bytes, int length) {
+        char[] hexChars = new char[length * 2];
+        for (int j = 0; j < length; j++) {
             int v = bytes[j] & 0xFF;
             hexChars[j * 2] = HEX_ARRAY[v >>> 4];
             hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
